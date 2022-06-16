909dd1883d30a7aaa42c96407e2192f60fd78b0f
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2022-06-16 11:33:15 +0530

    saving site-config settings

--- app/Controllers/Admin/Settings.php
+++ app/Controllers/Admin/Settings.php
@@ -8,7 +8,8 @@ use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Libraries\Utility;
 
-class Settings extends BaseController{
+class Settings extends BaseController
+{
 
 
     use ResponseTrait;
@@ -22,11 +23,53 @@ class Settings extends BaseController{
         return view('admin/admin_settings', $params);
     }
 
-    public function getSetting() {
+    public function getSetting()
+    {
         $post = $this->request->getPost('postdata');
         $postdata = json_decode($post);
         $settingsModel = new SettingsModel();
         $data = $settingsModel->getDataByName($postdata->name);
         return $this->respond($data);
+    }
+
+    public function updateSetting()
+    {
+        $post = $this->request->getPost('postdata');
+        $setting_name = $this->request->getPost('name');
+        $json = json_decode($post);
+        $settingsModel = new SettingsModel();
+        $value = [
+            'site_name' =>  $json->site_name,
+            'site_desc' => $json->site_desc,
+            'site_keywords' => $json->site_keywords,
+            'site_theme' => $json->site_theme,
+            'site_logo' => $json->site_logo,
+            'site_contact_email' => $json->site_contact_email,
+            'site_contact_phone' => $json->site_contact_phone,
+            'site_contact_mobile' => $json->site_contact_mobile,
+            'site_show_logo_only' => $json->site_show_logo_only,
+            'site_show_name_only' => $json->site_show_name_only,
+            'site_isblog' => $json->site_isblog,
+            'site_allow_comments' => $json->site_allow_comments,
+            'site_allow_comments_moderation' => $json->site_allow_comments_moderation,
+            'site_show_archive' => $json->site_show_archive,
+            'site_show_categories' => $json->site_show_categories,
+            'site_show_social_links' => $json->site_show_social_links,
+            'site_social_fb_url' => $json->site_social_fb_url,
+            'site_social_ig_url' => $json->site_social_ig_url,
+            'site_social_tw_url' => $json->site_social_tw_url
+        ];
+        $data = [
+            'setting_name' => $setting_name,
+            'setting_value' => json_encode($value)
+        ];
+        $setting = $settingsModel->getDataByName($setting_name);
+        if (sizeof($setting) > 0) {
+            $settingsModel->updateData($data);
+        } else {
+            $settingsModel->addData($data);
+        }
+
+        echo 'SUCCESS';
     }
 }
--- app/Views/admin/admin_settings.php
+++ app/Views/admin/admin_settings.php
@@ -19,12 +19,13 @@
                 if (result.length != 0) {
                     var siteConfig = JSON.parse(result[0].setting_value);
                     console.log(siteConfig);
-                    $('#site_name').val(siteConfig['site-name']);
-                    $('#site_desc').val(siteConfig['site-desc']);
-                    $('#site_keywords').val(siteConfig['site-keywords']);
-                    $('#site_theme').val(siteConfig['site-theme']);
-                    $('#site_logo').val(siteConfig['site-logo']);
+                    $('#site_name').val(siteConfig['site_name']);
+                    $('#site_desc').val(siteConfig['site_desc']);
+                    $('#site_keywords').val(siteConfig['site_keywords']);
+                    $('#site_theme').val(siteConfig['site_theme']);
+                    $('#site_logo').val(siteConfig['site_logo']);
                     $('#site_contact_email').val(siteConfig['site_contact_email']);
+                    $('#site_contact_phone').val(siteConfig['site_contact_phone']);
                     $('#site_contact_mobile').val(siteConfig['site_contact_mobile']);
                     $('#site_show_logo_only').prop('checked', siteConfig['site_show_logo_only']);
                     $('#site_show_name_only').prop('checked', siteConfig['site_show_name_only']);
@@ -44,6 +45,49 @@
             }
         });
     }
+
+    function save() {
+        var valid = $(document).validate();
+        if (!valid) return;
+
+        var postdata = {
+            'site_name': $('#site_name').val(),
+            'site_desc': $('#site_desc').val(),
+            'site_keywords': $('#site_keywords').val(),
+            'site_theme': $('#site_theme').val(),
+            'site_logo': $('#site_logo').val(),
+            'site_contact_email': $('#site_contact_email').val(),
+            'site_contact_phone': $('#site_contact_phone').val(),
+            'site_contact_mobile': $('#site_contact_mobile').val(),
+            'site_show_logo_only': $('#site_show_logo_only').is(':checked'),
+            'site_show_name_only': $('#site_show_name_only').is(':checked'),
+            'site_isblog': $('#site_isblog').is(':checked'),
+            'site_allow_comments': $('#site_allow_comments').is(':checked'),
+            'site_allow_comments_moderation': $('#site_allow_comments_moderation').is(':checked'),
+            'site_show_archive': $('#site_show_archive').is(':checked'),
+            'site_show_categories': $('#site_show_categories').is(':checked'),
+            'site_show_social_links': $('#site_show_social_links').is(':checked'),
+            'site_social_fb_url': $('#site_social_fb_url').val(),
+            'site_social_ig_url': $('#site_social_ig_url').val(),
+            'site_social_tw_url': $('#site_social_tw_url').val()
+        }
+        postdata = JSON.stringify(postdata);
+        $.ajax({
+            type: "POST",
+            url: "<?php echo base_url() . '/' . index_page() ?>/admin/settings/updateSetting",
+            data: "postdata=" + postdata + "&name=site-config",
+            success: function(result) {
+                if (result.indexOf('SUCCESS') >= 0) {
+                    console.log("Successfully saved!");
+                } else {
+                    console.log(result);
+                }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    }
 </script>
 <div class="container-fluid">
     <div class="row">
@@ -56,7 +100,7 @@
         </div>
         <div class="col-sm-6">
             <div class="mb-2">
-                <label for="site_desc" class="form-label"><?php echo lang('Default.desciption') ?></label>
+                <label for="site_desc" class="form-label"><?php echo lang('Default.site_desciption') ?></label>
                 <input type="text" id="site_desc" class="form-control required" maxlength="250">
                 <div class="required_input">Please enter some text</div>
             </div>
@@ -65,27 +109,20 @@
     <div class="row">
         <div class="col-sm-6">
             <div class="mb-2">
-                <label for="site_keywords" class="form-label"><?php echo lang('Default.keywords') ?></label>
+                <label for="site_keywords" class="form-label"><?php echo lang('Default.site_keywords') ?></label>
                 <input type="text" id="site_keywords" class="form-control required" maxlength="250">
                 <div class="required_input">Please enter some text</div>
             </div>
         </div>
         <div class="col">
-                        <div class="mb-2">
-                            <label for="f_pcgid" class="form-label"><?php echo lang('Default.theme') ?></label>
-                            <select id="f_pcgid" class="form-select">
-                                <option value="">Default</option>
-                                <option value="">Modern</option>
-                                <option value="">Classic</option>
-                             
-                            </select>
-                        </div>
-                    </div>
-        <div class="col-sm-6">
             <div class="mb-2">
                 <label for="site_theme" class="form-label"><?php echo lang('Default.theme') ?></label>
-                <input type="text" id="site_theme" class="form-control email">
-                <div class="invalid_email">Invalid Email</div>
+                <select id="site_theme" class="form-select">
+                    <option value="Default">Default</option>
+                    <option value="Modern">Modern</option>
+                    <option value="Classic">Classic</option>
+
+                </select>
             </div>
         </div>
     </div>
@@ -105,10 +142,17 @@
             </div>
         </div>
     </div>
-    <div class="col-sm-6">
-        <label for="site_contact_mobile" class="form-label"><?php echo lang('Default.contact_mobile') ?></label>
-        <input type="text" id="site_contact_mobile" class="form-control required" maxlength="250">
-        <div class="required_input">Please enter some text</div>
+    <div class="row">
+        <div class="col-sm-6">
+            <label for="site_contact_phone" class="form-label"><?php echo lang('Default.contact_phone') ?></label>
+            <input type="text" id="site_contact_phone" class="form-control required" maxlength="15">
+            <div class="required_input">Please enter some text</div>
+        </div>
+        <div class="col-sm-6">
+            <label for="site_contact_mobile" class="form-label"><?php echo lang('Default.contact_mobile') ?></label>
+            <input type="text" id="site_contact_mobile" class="form-control required" maxlength="15">
+            <div class="required_input">Please enter some text</div>
+        </div>
     </div>
     <div class="row">
         <div class="col-sm-4">
