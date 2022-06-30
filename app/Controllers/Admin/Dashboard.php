a42544a945f9da0892d0af42ac228c4e9a6ef40a
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2022-06-30 17:07:50 +0530

    dashboard statistics

--- app/Controllers/Admin/Dashboard.php
+++ app/Controllers/Admin/Dashboard.php
@@ -3,6 +3,10 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\CommentsModel;
+use App\Models\PagesModel;
+use App\Models\PostsModel;
+use App\Models\UsersModel;
 
 class Dashboard extends BaseController
 {
@@ -11,11 +15,29 @@ class Dashboard extends BaseController
         if (!$this->isLoggedIn()) {
             return redirect()->to(base_url() . '/' . index_page() . '/admin/login');
         }
+        $session_user = $_SESSION['user_fullname'];
 
         $params = [
             'page_title' => lang('Default.dashboard'),
-            'menu_id' => 'dashboard'
+            'menu_id' => 'dashboard',
+            'user_name' => $session_user
         ];
+        $pagesModel = new PagesModel();
+        $pageCount = $pagesModel->builder()->countAllResults();
+        $params['page_count'] = $pageCount;
+
+        $postsModel = new PostsModel();
+        $postCount = $postsModel->builder()->countAllResults();
+        $params['post_count'] = $postCount;
+
+        $usersModel = new UsersModel();
+        $userCount = $usersModel->builder()->countAllResults();
+        $params['user_count'] = $userCount;
+
+        $commentsModel = new CommentsModel();
+        $commentCount = $commentsModel->builder()->where(['cmt_published' => 0])->countAllResults();
+        $params['comment_count'] = $commentCount;
+
         return view('admin/admin_home', $params);
     }
 }
--- app/Views/admin/admin_home.php
+++ app/Views/admin/admin_home.php
@@ -1,37 +1,46 @@
 <?php $this->extend('admin/admin_template') ?>
 <?php $this->section('content') ?>
-
-<div class="row">
-    <div class="col">
+<h3><?= 'Welcome ' . $user_name ?></h3>
+<div class="row g-2">
+    <div class="col-sm col-md-6 col-lg-4">
         <div class="card">
-            <h5 class="card-header">Featured</h5>
+            <h5 class="card-header">Pages</h5>
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Create and Manage Site Pages</h5>
+                <p class="card-text"><?= $page_count ?> Pages</p>
+                <a href="<?= base_url() . '/' . index_page() ?>/admin/pages" class="btn btn-primary">Open</a>
             </div>
         </div>
     </div>
-    <div class="col">
+    <div class="col-sm col-md-6 col-lg-4">
         <div class="card">
-            <h5 class="card-header">Featured</h5>
+            <h5 class="card-header">Posts</h5>
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Create and Manage Posts</h5>
+                <p class="card-text"><?= $post_count ?> Posts</p>
+                <a href="<?= base_url() . '/' . index_page() ?>/admin/posts" class="btn btn-primary">Open</a>
             </div>
         </div>
     </div>
-    <div class="col">
+    <div class="col-sm col-md-6 col-lg-4">
         <div class="card">
-            <h5 class="card-header">Featured</h5>
+            <h5 class="card-header">Users</h5>
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Create and Manage Users</h5>
+                <p class="card-text"><?= $user_count ?> Users</p>
+                <a href="<?= base_url() . '/' . index_page() ?>/admin/users" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+        <div class="card">
+            <h5 class="card-header">Comments</h5>
+            <div class="card-body">
+                <h5 class="card-title">Publish and Manage Comments</h5>
+                <p class="card-text"><?= $comment_count ?> Unpublished Comments</p>
+                <a href="<?= base_url() . '/' . index_page() ?>/admin/posts" class="btn btn-primary">Open</a>
             </div>
         </div>
     </div>
 </div>
-
 <?php $this->endSection() ?>
