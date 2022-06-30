d3d59b165321bb2cc051c7d7896d52f959c68452
Rajesh Menon <rajeshmenon@rennovationsoftware.com>
2022-06-30 17:19:33 +0530

    font size changed

--- app/Views/admin/admin_home.php
+++ app/Views/admin/admin_home.php
@@ -1,13 +1,13 @@
 <?php $this->extend('admin/admin_template') ?>
 <?php $this->section('content') ?>
-<h3><?= 'Welcome ' . $user_name ?></h3>
+<h4><?= 'Welcome ' . $user_name ?></h4>
 <div class="row g-2">
     <div class="col-sm col-md-6 col-lg-4">
         <div class="card">
             <h5 class="card-header">Pages</h5>
             <div class="card-body">
                 <h5 class="card-title">Create and Manage Site Pages</h5>
-                <p class="card-text"><?= $page_count ?> Pages</p>
+                <p class="card-text fw-light"><?= $page_count ?> Pages</p>
                 <a href="<?= base_url() . '/' . index_page() ?>/admin/pages" class="btn btn-primary">Open</a>
             </div>
         </div>
@@ -17,7 +17,7 @@
             <h5 class="card-header">Posts</h5>
             <div class="card-body">
                 <h5 class="card-title">Create and Manage Posts</h5>
-                <p class="card-text"><?= $post_count ?> Posts</p>
+                <p class="card-text fw-light"><?= $post_count ?> Posts</p>
                 <a href="<?= base_url() . '/' . index_page() ?>/admin/posts" class="btn btn-primary">Open</a>
             </div>
         </div>
@@ -27,7 +27,7 @@
             <h5 class="card-header">Users</h5>
             <div class="card-body">
                 <h5 class="card-title">Create and Manage Users</h5>
-                <p class="card-text"><?= $user_count ?> Users</p>
+                <p class="card-text fw-light"><?= $user_count ?> Users</p>
                 <a href="<?= base_url() . '/' . index_page() ?>/admin/users" class="btn btn-primary">Open</a>
             </div>
         </div>
@@ -37,7 +37,7 @@
             <h5 class="card-header">Comments</h5>
             <div class="card-body">
                 <h5 class="card-title">Publish and Manage Comments</h5>
-                <p class="card-text"><?= $comment_count ?> Unpublished Comments</p>
+                <p class="card-text fw-light"><?= $comment_count ?> Unpublished Comments</p>
                 <a href="<?= base_url() . '/' . index_page() ?>/admin/posts" class="btn btn-primary">Open</a>
             </div>
         </div>
